78e9404cee4382eb5f734a487ff6e236444f117a
Eugene The Dream <ek@remote>
2024-04-12 10:05:33 -0700

    Add retries to cache

--- src/cache.rs
+++ src/cache.rs
@@ -1,7 +1,7 @@
 use crate::types::*;
+use crate::with_retries;
 
-const READ_TIMEOUT: std::time::Duration = std::time::Duration::from_millis(500);
-const WRITE_TIMEOUT: std::time::Duration = std::time::Duration::from_millis(1000);
+const REDIS_TIMEOUT: std::time::Duration = std::time::Duration::from_millis(5000);
 const CACHE_EXPIRATION: std::time::Duration = std::time::Duration::from_secs(60);
 
 const TARGET: &str = "cache";
@@ -14,16 +14,11 @@ pub(crate) async fn get_last_block_height(
     redis_client: redis::Client,
     chain_id: ChainId,
 ) -> Option<BlockHeight> {
-    let mut connection = redis_client
-        .get_multiplexed_async_connection_with_timeouts(READ_TIMEOUT, READ_TIMEOUT)
-        .await
-        .ok()?;
-    let key = format!("meta:{}:last_block", chain_id);
-    redis::cmd("GET")
-        .arg(&key)
-        .query_async(&mut connection)
-        .await
-        .ok()
+    let res: redis::RedisResult<BlockHeight> = with_retries!(redis_client, |connection| async {
+        let key = format!("meta:{}:last_block", chain_id);
+        redis::cmd("GET").arg(&key).query_async(connection).await
+    });
+    res.ok()
 }
 
 pub(crate) async fn get_block_and_last_block_height(
@@ -31,17 +26,17 @@ pub(crate) async fn get_block_and_last_block_height(
     chain_id: ChainId,
     block_height: BlockHeight,
 ) -> redis::RedisResult<(Option<String>, Option<BlockHeight>)> {
-    let mut connection = redis_client
-        .get_multiplexed_async_connection_with_timeouts(READ_TIMEOUT, READ_TIMEOUT)
-        .await?;
-
-    let res: (Option<String>, Option<String>) = redis::pipe()
-        .cmd("GET")
-        .arg(block_key(chain_id, block_height))
-        .cmd("GET")
-        .arg(format!("meta:{}:last_block", chain_id))
-        .query_async(&mut connection)
-        .await?;
+    let res: redis::RedisResult<(Option<String>, Option<String>)> =
+        with_retries!(redis_client, |connection| async {
+            redis::pipe()
+                .cmd("GET")
+                .arg(block_key(chain_id, block_height))
+                .cmd("GET")
+                .arg(format!("meta:{}:last_block", chain_id))
+                .query_async(connection)
+                .await
+        });
+    let res = res?;
 
     Ok((res.0, res.1.map(|s| s.parse().unwrap())))
 }
@@ -53,17 +48,16 @@ pub(crate) async fn set_block(
     block_height: BlockHeight,
     block: &str,
 ) -> Result<(), redis::RedisError> {
-    let mut connection = redis_client
-        .get_multiplexed_async_connection_with_timeouts(WRITE_TIMEOUT, WRITE_TIMEOUT)
-        .await?;
-    let key = block_key(chain_id, block_height);
-    redis::cmd("SET")
-        .arg(&key)
-        .arg(block)
-        .arg("EX")
-        .arg(CACHE_EXPIRATION.as_secs())
-        .query_async(&mut connection)
-        .await
+    with_retries!(redis_client, |connection| async {
+        let key = block_key(chain_id, block_height);
+        redis::cmd("SET")
+            .arg(&key)
+            .arg(block)
+            .arg("EX")
+            .arg(CACHE_EXPIRATION.as_secs())
+            .query_async(connection)
+            .await
+    })
 }
 
 pub(crate) fn set_multiple_blocks_async(
@@ -85,17 +79,48 @@ async fn set_multiple_blocks(
     chain_id: ChainId,
     blocks: Vec<(BlockHeight, Option<String>)>,
 ) -> Result<(), redis::RedisError> {
-    let mut connection = redis_client
-        .get_multiplexed_async_connection_with_timeouts(WRITE_TIMEOUT, WRITE_TIMEOUT)
-        .await?;
-    let mut pipe = redis::pipe();
-    for (block_height, block) in blocks {
-        let key = block_key(chain_id, block_height);
-        pipe.cmd("SET")
-            .arg(&key)
-            .arg(block.unwrap_or_default())
-            .arg("EX")
-            .arg(CACHE_EXPIRATION.as_secs());
-    }
-    pipe.query_async(&mut connection).await
+    with_retries!(redis_client, |connection| async {
+        let mut pipe = redis::pipe();
+        for (block_height, block) in &blocks {
+            let key = block_key(chain_id, *block_height);
+            pipe.cmd("SET")
+                .arg(&key)
+                .arg(block.as_ref().map(|s| s.as_str()).unwrap_or_default())
+                .arg("EX")
+                .arg(CACHE_EXPIRATION.as_secs());
+        }
+        pipe.query_async(connection).await
+    })
+}
+
+#[macro_export]
+macro_rules! with_retries {
+    ($client: expr, $f_async: expr) => {
+        {
+            let mut delay = tokio::time::Duration::from_millis(100);
+            let max_retries = 7;
+            let mut i = 0;
+            loop {
+                let connection =
+                    $client.get_multiplexed_async_connection_with_timeouts(REDIS_TIMEOUT, REDIS_TIMEOUT)
+                    .await;
+                let err = match connection {
+                    Ok(mut connection) => {
+                        match $f_async(&mut connection).await {
+                            Ok(v) => break Ok(v),
+                            Err(err) => err,
+                        }
+                    }
+                    Err(err) => err,
+                };
+                tracing::log::error!(target: "redis", "Attempt #{}: connection error {}", i, err);
+                tokio::time::sleep(delay).await;
+                delay *= 2;
+                i += 1;
+                if i >= max_retries {
+                    break Err(err);
+                }
+            }
+        }
+    };
 }
